3e079d4367b3f8381b0f18d236ad25f67832e19f
Malachi Ivey <zarekbusiness@gmail.com>
2020-01-26 18:14:22 -0500

    added blog and about pages

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogPage = () => {
+    return (
+        <div>
+            <h1>Blog coming soon...</h1>
+        </div>
+    )
+}
+
+export default BlogPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Hi, I'm Malachi Ivey.</h1>
+            <h2>I'm currently a Software Developer in Atlanta. When I'm not coding I'm exploring the city and  discovering new technolgies.</h2>
+        </div>
+    )
+}
+
+export default IndexPage;
