c681879b56279fdacc64c457f1c53d042c0bfce9
Malachi Ivey <zarekbusiness@gmail.com>
2020-02-12 21:59:45 -0500

    Formatting fix.

--- src/components/header.js
+++ src/components/header.js
@@ -11,32 +11,32 @@ class Header extends Component {
         super(props, context);
     
         this.state = {
-          visible: false
+            visible: false
         };
     
         this.handleMouseDown = this.handleMouseDown.bind(this);
         this.toggleMenu = this.toggleMenu.bind(this);
-      }
+    }
     
-      toggleMenu() {
+    toggleMenu() {
         this.setState({
-          visible: !this.state.visible
+            visible: !this.state.visible
         });
-      }
+    }
     
-      handleMouseDown(e) {
+    handleMouseDown(e) {
         this.toggleMenu();
-      }
+    }
     
-      render() {
-      return (
-        <div className="navigation-bar-container">
-          <NavMenu handleMouseDown={this.handleMouseDown} menuVisibility={this.state.visible} />
-          <Link className={headerStyles.title}to="/">M</Link>
-          <NavButton handleMouseDown={this.handleMouseDown} />
-          <div id="theMenu"></div>
-        </div>
-      );
+    render() {
+        return (
+            <div className="navigation-bar-container">
+                <NavMenu handleMouseDown={this.handleMouseDown} menuVisibility={this.state.visible} />
+                <Link className={headerStyles.title}to="/">M</Link>
+                <NavButton handleMouseDown={this.handleMouseDown} />
+                <div id="theMenu"></div>
+            </div>
+        );
     }
 }
 
--- src/components/navMenu.js
+++ src/components/navMenu.js
@@ -7,21 +7,21 @@ class navMenu extends Component {
         let visibility = "hide";
     
         if (this.props.menuVisibility) {
-          visibility = "show";
+            visibility = "show";
         }
     
         return (
-          <div id="flyoutMenu" className={visibility}>
-            <button className="closeNav" onMouseDown={this.props.handleMouseDown}>-</button>
-            <div className="box">
-              <ul>
-                <li className="nav-link"><Link className="nav-link" to="/">HOME</Link></li>
-                <li className="nav-link"><Link className="nav-link" to="/about">ABOUT</Link></li>
-                <li className="nav-link"><Link className="nav-link" to="/blog">BLOG</Link></li>
-                <Link to="/links"></Link>
-              </ul>
+            <div id="flyoutMenu" className={visibility}>
+                <button className="closeNav" onMouseDown={this.props.handleMouseDown}>-</button>
+                <div className="box">
+                <ul>
+                    <li className="nav-link"><Link className="nav-link" to="/">Home</Link></li>
+                    <li className="nav-link"><Link className="nav-link" to="/about">About</Link></li>
+                    <li className="nav-link"><Link className="nav-link" to="/blog">Blog</Link></li>
+                    <Link to="/links"></Link>
+                </ul>
+                </div>
             </div>
-          </div>
         );
       }
 }
