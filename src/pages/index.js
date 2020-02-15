6ab1951c4a2b6c9fbe8e4c2dea9dc2dd40fb4bdf
Malachi Ivey <zarekbusiness@gmail.com>
2020-02-14 22:27:34 -0500

    Landing page styling updates.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from "react"
 
 import Layout from '../components/layout'
+import indexStyles from '../styles/landing.module.scss'
 
 const IndexPage = () => {
     return (
         <Layout>
-            <h1>Hi, I'm Malachi Ivey.</h1>
-            <h2>I'm currently a Software Developer in Atlanta. When I'm not coding I'm exploring the city and  discovering new technolgies.</h2>
+            <div className="index-container">
+                <h1 className={indexStyles.hello}>Hi, I'm Malachi Ivey.</h1>
+                <h2 className={indexStyles.info}>I'm currently a Software Developer in Atlanta. When I'm not coding I'm exploring the city and  discovering new technolgies.</h2>
+            </div> 
         </Layout>
     )
 }
