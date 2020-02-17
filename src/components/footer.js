7f05e660fe9abbe907cfb7bda6218a38a115bc78
Malachi Ivey <zarekbusiness@gmail.com>
2020-02-16 19:51:23 -0500

    Adding social icons to footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
     return (
         <footer>
+            <div>
+                <a href="https://www.linkedin.com/in/malachiivey/"><FontAwesomeIcon icon = { faLinkedin }></FontAwesomeIcon></a>
+                <a href="https://www.linkedin.com/in/malachiivey/"><FontAwesomeIcon icon = { faGithub }></FontAwesomeIcon></a>
+                <a href="https://www.linkedin.com/in/malachiivey/"><FontAwesomeIcon icon = { faEnvelopeSquare }></FontAwesomeIcon></a>
+            </div>
             <p>Created by Malachi Ivey</p>
         </footer>
     )
