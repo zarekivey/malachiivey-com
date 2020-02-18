972b910d2eca6883ad9ff79f07e05b75aa461f82
Malachi Ivey <zarekbusiness@gmail.com>
2020-02-17 20:34:08 -0500

    Added and implemented about.module.scss.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import aboutStyles from '../styles/about.module.scss'
 
 const AboutPage = () => {
     return (
         <Layout>
-            <h1>About page coming soon.</h1>
+            <div className="index-container">
+                <h1 className={aboutStyles.title}>About page coming soon...</h1>
+            </div>
         </Layout>
     )
 }
