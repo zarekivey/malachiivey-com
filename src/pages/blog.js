246811ffa42b8bc2305de5dcccb2ea01ab0b49ce
Malachi Ivey <zarekbusiness@gmail.com>
2020-02-17 19:30:10 -0500

    Adding blog.module.scss

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import blogStyles from '../styles/blog.module.scss'
 
 const BlogPage = () => {
     return (
         <Layout>
-            <h1>Blog coming soon...</h1>
+            <div className="index-container">
+                <h1 className={blogStyles.title}>Blog coming soon...</h1>
+            </div>
         </Layout>
     )
 }
