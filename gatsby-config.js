e54ccc8020960d323d57ded829e25a91e02c21c5
Malachi Ivey <zarekbusiness@gmail.com>
2020-02-19 13:58:37 -0500

    Added environment variables and gatsby-source-contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,13 @@
 
 module.exports = {
   plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
     'gatsby-plugin-sass'
   ]
 }
