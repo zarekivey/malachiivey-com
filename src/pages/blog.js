c47f8c0f40fe1cc0e215bd9190f1380942e8970f
Malachi Ivey <zarekbusiness@gmail.com>
2020-02-20 14:50:09 -0500

    Made a Graphql query to get data from Contentful.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,44 @@
 import React from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby' 
 
 import Layout from '../components/layout'
 import blogStyles from '../styles/blog.module.scss'
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allContentfulBlogPost (
+            sort: {
+            fields: publishedDate,
+            order: DESC
+            }
+        ) {
+            edges {
+            node {
+                title
+                slug
+                publishedDate(formatString: "MMMM Do, YYYY")
+            }
+            }
+        }
+        }
+    `)
     return (
         <Layout>
             <div className="index-container">
-                <h1 className={blogStyles.title}>Blog coming soon...</h1>
+                <h1 className={blogStyles.title}>Welcome to my blog!</h1>
+                <ol>
+                    {data.allContentfulBlogPost.edges.map((edge) => {
+                        return (
+                            <li>
+                                <Link to={`/blog/${edge.node.slug}`}>
+                                    <h2>{edge.node.title}</h2>
+                                    <p>{edge.node.publishedDate}</p>
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ol>
             </div>
         </Layout>
     )
