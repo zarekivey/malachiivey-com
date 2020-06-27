032b9246b88164f329fb7c52bdd3c7369e3aabd0
Malachi Ivey <zarekbusiness@gmail.com>
2020-06-27 14:21:56 -0400

    Added githubBlock.js component.

---
+++ src/components/githubBlock.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const GithubBlock = () => {
+  return (
+    <div>
+      <h1>This is the github block.</h1>
+    </div>
+  )
+}
+
+export default GithubBlock
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,54 @@
 import React from "react"
 
-import Layout from '../components/layout'
-import indexStyles from '../styles/landing.module.scss'
+import Layout from "../components/layout"
+import GithubBlock from "../components/githubBlock"
+import indexStyles from "../styles/landing.module.scss"
+import { useStaticQuery } from "gatsby"
 
 const IndexPage = () => {
-    return (
-        <Layout>
-            <div className="index-container">
-                <h1 className={indexStyles.hello}>Hi, I'm Malachi Ivey.</h1>
-                <h2 className={indexStyles.info}>I'm currently a Software Developer in Atlanta. When I'm not coding I'm exploring the city and  discovering new technolgies.</h2>
-            </div> 
-        </Layout>
-    )
+  // const data = useStaticQuery(`graphql
+  // query {
+  //     github {
+  //       viewer {
+  //         repositories(
+  //           first: 6
+  //           isFork: false
+  //           privacy: PUBLIC
+  //           orderBy: { field: STARGAZERS, direction: DESC }
+  //         ) {
+  //           edges {
+  //             node {
+  //               id
+  //               name
+  //               url
+  //               description
+  //               forkCount
+  //               stargazers {
+  //                 totalCount
+  //               }
+  //               primaryLanguage {
+  //                 name
+  //                 color
+  //               }
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+  return (
+    <Layout>
+      <div className="index-container">
+        <h1 className={indexStyles.hello}>Hi, I'm Malachi Ivey.</h1>
+        <h2 className={indexStyles.info}>
+          I'm currently a Software Developer in Atlanta. When I'm not coding I'm
+          exploring the city and discovering new technolgies.
+        </h2>
+      </div>
+      <GithubBlock />
+    </Layout>
+  )
 }
 
-export default IndexPage;
+export default IndexPage
