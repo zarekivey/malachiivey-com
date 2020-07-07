b1b46de63f33540a29310e13aaa7395bbecf0887
Malachi Ivey <zarekbusiness@gmail.com>
2020-07-07 12:39:29 -0400

    Blog template updates

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,6 +2,8 @@ import React from "react"
 import { graphql } from "gatsby"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
+import blogStyles from "../styles/blog.module.scss"
+
 import Layout from "../components/layout"
 
 export const query = graphql`
@@ -19,19 +21,24 @@ export const query = graphql`
 const Blog = props => {
   const options = {
     renderNode: {
-      "embedded-asset-block": (node) => {
-        const alt = node.data.target.fields.title['en-US']
-        const url = node.data.target.fields.file['en-US'].url
+      "embedded-asset-block": node => {
+        const alt = node.data.target.fields.title["en-US"]
+        const url = node.data.target.fields.file["en-US"].url
         return <img alt={alt} src={url} />
-      }
-    }
+      },
+    },
   }
 
   return (
     <Layout>
-      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <h1 className={blogStyles.title}>
+        {props.data.contentfulBlogPost.title}
+      </h1>
       <p>{props.data.contentfulBlogPost.publishedDate}</p>
-      {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
+      {documentToReactComponents(
+        props.data.contentfulBlogPost.body.json,
+        options
+      )}
     </Layout>
   )
 }
