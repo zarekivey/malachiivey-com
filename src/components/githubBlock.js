becf4632824d2e575ed44ab4e29d0def2e620683
Malachi Ivey <zarekbusiness@gmail.com>
2020-08-12 09:05:57 -0400

    githubBlock graphql query

--- src/components/githubBlock.js
+++ src/components/githubBlock.js
@@ -1,9 +1,31 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 const GithubBlock = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      github {
+        viewer {
+          repositories(first: 4, isFork: false, privacy: PUBLIC) {
+            edges {
+              node {
+                id
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
   return (
     <div>
       <h1>This is the github block.</h1>
+      <ol>
+        {data.github.viewer.repositories.edges.map(edge => {
+          return <li>{edge.node.name}</li>
+        })}
+      </ol>
     </div>
   )
 }
