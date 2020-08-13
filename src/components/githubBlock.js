3032caa67f750374b2f206277b3d0bbeb7b1fc40
Malachi Ivey <zarekbusiness@gmail.com>
2020-08-13 12:22:46 -0400

    Added githubblock.module.scss + style changes.

--- src/components/githubBlock.js
+++ src/components/githubBlock.js
@@ -1,11 +1,12 @@
 import React from "react"
 
 import GithubCard from "./githubCard"
+import githubBlockStyles from "../styles/githubblock.module.scss"
 
 const GithubBlock = () => {
   return (
-    <div>
-      <h1>This is the github block.</h1>
+    <div className="index-container">
+      <h1 className={githubBlockStyles.title}>Check out my Github projects!</h1>
       <GithubCard />
     </div>
   )
