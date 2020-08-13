c26994fc0fbd4398d1bb43bd4ac5563de434183d
Malachi Ivey <zarekbusiness@gmail.com>
2020-08-13 17:53:14 -0400

    githubBlock style updates.

--- src/components/githubBlock.js
+++ src/components/githubBlock.js
@@ -6,7 +6,10 @@ import githubBlockStyles from "../styles/githubblock.module.scss"
 const GithubBlock = () => {
   return (
     <div className="index-container">
-      <h1 className={githubBlockStyles.title}>Check out my Github projects!</h1>
+      <h1 className={githubBlockStyles.title}>What am I working on?</h1>
+      <h2 className={githubBlockStyles.info}>
+        Checkout some of my github repositories.
+      </h2>
       <GithubCard />
     </div>
   )
