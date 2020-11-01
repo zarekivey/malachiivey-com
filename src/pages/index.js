a734e24f110252f6eda489ef72be2aa05fddfa63
Malachi Ivey <zarekbusiness@gmail.com>
2020-11-01 10:25:52 -0500

    removed contact,js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 
 import Layout from "../components/layout"
 import GithubBlock from "../components/githubBlock"
-import Contact from "../components/contact"
 import indexStyles from "../styles/pages/landing.module.scss"
 
 const IndexPage = () => {
@@ -22,7 +21,6 @@ const IndexPage = () => {
         <h1 className={indexStyles.hello}>About this website.</h1>
         <h2 className={indexStyles.info}>I created this site as an upgrade from my previous portfolio. Though the simplicity of pure HTML, CSS, and JavaScript was satisfying to build with, I wanted to simplify the addition of more complex features like API Integrations and a blog. I chose to make this website with GatsbyJS as it has the ability to preload content, and everything you need is built in to the framework. This makes the site easy to scale without skimping on speed. Beyond the technology used, I use this site as a way to document my tech journey and as a way to connect with other developers and possible employers. Want to learn more about how I made this website? I wrote a blog post where I take a deep dive into the process: the technologies, steps, and best practices I used to create it *here*. </h2>
       </div>
-      <Contact />
     </Layout>
   )
 }
